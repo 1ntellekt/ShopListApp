c8c92c02dfbf4779f3645f97be3806007459da11
1ntellekt <pitetvasa@gmail.com>
2022-05-16 00:18:43 +0600

    init domain

---
+++ app/src/main/java/com/example/shoplistapp/domain/GetShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoplistapp.domain
+
+class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
+    fun getShopItem(id:Int):ShopItem {
+        return shopListRepository.getShopItem(id)
+    }
+}
---
+++ app/src/main/java/com/example/shoplistapp/domain/GetShopListUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoplistapp.domain
+
+class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
+    fun getShopList():List<ShopItem>{
+        return shopListRepository.getShopList()
+    }
+}
