44962030bb1a4b09091ba330c755cad4ae0d7f28
1ntellekt <pitetvasa@gmail.com>
2022-05-21 17:06:39 +0600

    add listAdapter & auto-callbacks

---
+++ app/src/main/java/com/example/shoplistapp/presentation/ShopListAdapter.kt
@@ -0,0 +1,86 @@
+package com.example.shoplistapp.presentation
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoplistapp.R
+import com.example.shoplistapp.domain.ShopItem
+import java.lang.RuntimeException
+
+class ShopListAdapter : ListAdapter<ShopItem,ShopItemViewHolder>(ShopItemDiffCallback()) {
+
+/*    var shopList = listOf<ShopItem>()
+        set(value) {
+            val callback = ShopListDiffCallback(shopList,value)
+            val diffResult = DiffUtil.calculateDiff(callback)
+            diffResult.dispatchUpdatesTo(this)
+            field = value
+        }*/
+
+    var onShopItemLongClickListener:((ShopItem)->Unit)? = null
+    var onShopItemClickListener:((ShopItem)->Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        Log.d("tagAdapter", "onCreateViewHolder()")
+        val layoutId = when (viewType) {
+            VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
+            VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
+            else -> throw RuntimeException("Unknown view type: $viewType")
+        }
+
+        return ShopItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = getItem(position)
+        holder.apply {
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            itemView.setOnLongClickListener {
+                onShopItemLongClickListener?.invoke(shopItem)
+                true
+            }
+            itemView.setOnClickListener{
+                onShopItemClickListener?.invoke(shopItem)
+            }
+/*            if (shopItem.enable){
+                tvName.setTextColor(ContextCompat.getColor(itemView.context,R.color.black))
+            }*/
+        }
+    }
+
+    // set default values when viewHolder recycled
+/*    override fun onViewRecycled(holder: ShopItemViewHolder) {
+        super.onViewRecycled(holder)
+        holder.apply {
+            tvCount.text = ""
+            tvName.text = ""
+            tvName.setTextColor(ContextCompat.getColor(itemView.context,R.color.white))
+            tvCount.setTextColor(ContextCompat.getColor(itemView.context,R.color.white))
+        }
+    }*/
+
+    override fun getItemViewType(position: Int): Int {
+        val item = getItem(position)
+        return if (item.enable) {
+            VIEW_TYPE_ENABLED
+        } else {
+            VIEW_TYPE_DISABLED
+        }
+    }
+
+    companion object {
+        const val VIEW_TYPE_ENABLED = 0
+        const val VIEW_TYPE_DISABLED = 1
+        const val MAX_POOL_SIZE = 15
+    }
+
+}
